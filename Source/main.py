bb173a307258daa0b38f51f3ee6aab4907c4cc40
ngoncalves <ngoncalves@inovvati.com.br>
2025-11-28 10:52:39 -0400

    #007 - Configuração modular em Cogs dos comandos

--- Source/main.py
+++ Source/main.py
@@ -4,40 +4,43 @@ import logging
 from dotenv import load_dotenv
 import os
 
+# Carrega o arquivo .env e lê o token inserido nele
 load_dotenv()
 botToken = os.getenv('DISCORD_TOKEN')
 
+# Verifica a existencia da pasta cogs
+if not os.path.isdir('cogs'):
+    os.mkdir('cogs')
+
+# Definição de arquivo de logs
 logDeExecucao = logging.FileHandler(filename='discordBot.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+# Define as Cogs a serem carregadas
+cogsToLoad = [
+    'cogs.basicCommands'
+]
+
+# Define o prefixo do bot e seus intents
 bot = commands.Bot(command_prefix='cb/', intents=intents)
+
+async def loadCogs():
+    for cogName in cogsToLoad:
+        try:
+            await bot.load_extension(cogName)
+            print(f"Cog '{cogName}' loaded")
+        except commands.ExtensionNotFound:
+            print(f"Erro: Cog '{cogName}' não foi encontrado.")
+        except Exception as e:
+            print(f"Falha ao carregar Cog '{cogName}': {type(e).__name__}: {e}")
+            logging.error(f"Falha ao carregar Cog '{cogName}': {e}")
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} ativo e operante!")
-    await bot.get_channel(962854221244411964).send("Estou ativo")
-
-@bot.event
-async def on_member_join(member):
-    await member.send(f"Bem vindo {member.name}")
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    
-    if "cholabot" in message.content.lower() and "ola" in message.content.lower():
-        await message.channel.send(f"Ola {message.author.mention} !!!")
-
-    await bot.process_commands(message)
-
-@bot.event
-async def on_voice_state_update(member, before, after):
-    user_id = member.id
-
-    if before.channel is None and after.channel is not None:
-        await bot.get_channel(962854221244411964).send(f"{member.name} entrou em {after.channel}")
+    print(f'Bot {bot.user} Funcionando')
+    await loadCogs()
+    print('Chola bot preparado e operante!')
 
 bot.run(botToken, log_handler=logDeExecucao, log_level=logging.DEBUG)
